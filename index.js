58752134d19ffb6876615ce0dd805a36ccb4288e
jesse-morales <moral.jesse@outlook.com>
2022-09-30 22:55:53 -0600

    Refactor main

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs/promises')
 
-async function start() {
+async function start(website) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto("https://www.cnn.com/")
+    await page.goto(website)
     //await page.screenshot({path: "cnn.png", fullPage:true})
+    text()
+    photo()
+
+    await browser.close()
+}
+
+async function text() {
 
     const headline = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > a > h2")).map(x => x.textContent)
     })
     await fs.writeFile("headline.txt", headline.join("\r\n"))
 
+    await page.click("")
+    const nexPage = await page.$eval("", el => el.textContent)
+
     const headlineBody = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("")).map(x => x.textContent)
     })
     await fs.writeFile("headlineBody.txt", headline.join("\r\n"))
 
-    const headlinePhoto = page.$$eval("", (imgs) => {
-        return imgs.map(x => x.src)
-    })
-
-    await browser.close()
 }
 
 start()
