b5a656656936ff926132d1cb0d2d74ba3719280d
jesse-morales <moral.jesse@outlook.com>
2022-10-01 17:33:27 -0600

    efactor text retrieval

--- index.js
+++ index.js
@@ -6,27 +6,31 @@ async function start(website) {
     const page = await browser.newPage()
     await page.goto(website)
     //await page.screenshot({path: "cnn.png", fullPage:true})
-    text()
+    headerText()
     photo()
+    bodyText()
 
     await browser.close()
 }
 
-async function text() {
+async function headerText() {
 
     const headline = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > a > h2")).map(x => x.textContent)
     })
     await fs.writeFile("headline.txt", headline.join("\r\n"))
 
+}
+
+async function bodyText() {
+
     await page.click("")
-    const nexPage = await page.$eval("", el => el.textContent)
+    const nextPage = await page.$eval("", el => el.textContent)
 
     const headlineBody = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("")).map(x => x.textContent)
     })
-    await fs.writeFile("headlineBody.txt", headline.join("\r\n"))
-
+    await fs.writeFile("headlineBody.txt", headlineBody.join("\r\n"))
 }
 
-start()
+start("https://www.cnn.com")
