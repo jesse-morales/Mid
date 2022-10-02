66de53b1579583d32cc0275c3bbe925114e36cf4
jesse-morales <moral.jesse@outlook.com>
2022-10-01 18:44:55 -0600

    Add retrieval paths

--- index.js
+++ index.js
@@ -9,33 +9,27 @@ async function start(website) {
     headerText()
     headerPhoto()
     bodyText()
-
     await browser.close()
 }
 
 async function headerText() {
-
     const headline = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > a > h2")).map(x => x.textContent)
     })
     await fs.writeFile("headline.txt", headline.join("\r\n"))
-
 }
 
 async function headerPhoto() {
-   const photo = await page.evaluate(() => {
-        return document.querySelectorAll("").map(x => x.src)
+    await page.click("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > div > div.media > a")
+    const photo = await page.evaluate(() => {
+        return document.querySelectorAll("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > div > div.media > a > img").map(x => x.src)
     }) 
-    await fs.writeFile("headerPhoto.png")
+    await fs.writeFile("headerPhoto.png", photo)
 }
 
 async function bodyText() {
-
-    await page.click("")
-    const nextPage = await page.$eval("", el => el.textContent)
-
     const headlineBody = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll("")).map(x => x.textContent)
+        return Array.from(document.querySelectorAll("body > div.layout__content-wrapper.layout-with-rail__content-wrapper > section.layout__wrapper.layout-with-rail__wrapper > section.layout__main-wrapper.layout-with-rail__main-wrapper > section.layout__main.layout-with-rail__main > article > section > main > div.article__content-container > div.article__content > p:nth-child(2)")).map(x => x.textContent)
     })
     await fs.writeFile("headlineBody.txt", headlineBody.join("\r\n"))
 }
