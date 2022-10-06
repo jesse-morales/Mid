7ee6f8b573f4f6b22c6b0efc337ce836705221ab
jesse-morales <moral.jesse@outlook.com>
2022-10-06 13:52:47 -0600

    Fix failed header retrieval integration

--- index.js
+++ index.js
@@ -20,14 +20,15 @@ async function headerText(page) {
 }
 
 async function headerPhoto(page) {
-    await page.click("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > div > div.media > a")
-    const photo = await page.evaluate(() => {
-        return document.querySelectorAll("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > div > div.media > a > img").map(x => x.src)
-    }) 
-    await fs.writeFile("headerPhoto.png", photo)
+    const photo = page.$$eval("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > div > div.media > a > img", (imgs) => {
+        return imgs.map(x => x.src)
+    })
+    const imgpage = await page.goto(photo)
+    await fs.writeFile("headerphoto.jpg", await imgpage.buffer())
 }
 
 async function bodyText(page) {
+    await page.click("#homepage1-zone-1 > div.l-container > div > div.column.zn__column--idx-0 > ul > li:nth-child(1) > article > a")
     const headlineBody = await page.evaluate(() => {
         return Array.from(document.querySelectorAll("body > div.layout__content-wrapper.layout-with-rail__content-wrapper > section.layout__wrapper.layout-with-rail__wrapper > section.layout__main-wrapper.layout-with-rail__main-wrapper > section.layout__main.layout-with-rail__main > article > section > main > div.article__content-container > div.article__content > p:nth-child(2)")).map(x => x.textContent)
     })
